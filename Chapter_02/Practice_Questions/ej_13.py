36c534a249cd9f2a936604c1c6f55a9651d2cfd6
GSantos23 <santosgerson64@gmail.com>
2019-07-23 21:58:35 -0600

    Add program to question 13

---
+++ Chapter_02/Practice_Questions/ej_13.py
@@ -0,0 +1,20 @@
+'''
+Write a short program that prints the numbers 1 to 10 using a for loop.
+Then write an equivalent program that prints the numbers 1 to 10 using
+a while loop.
+'''
+
+#'''
+print('Using for loop')
+for  i in range(1,11):
+    print(i, end=' ')       # end='', allow to print in the same line
+
+print()
+
+print('Using while loop')
+j = 0
+while j < 11:
+    print(j, end=' ')
+    j = j + 1
+
+print()
