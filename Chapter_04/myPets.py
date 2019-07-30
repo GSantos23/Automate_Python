c5022a8c5f87225563acf18351d4f73b99a077d0
GSantos23 <santosgerson64@gmail.com>
2019-07-30 14:38:45 -0600

    Move to Samples/

--- Chapter_04/myPets.py
+++
@@ -1,7 +0,0 @@
-myPets = ['Zophie', 'Pooka', 'Fat-Tail']
-print('Enter a pet name:')
-name = input()
-if name not in myPets:
-    print('I do not have a pet named ' + name)
-else:
-    print(name + ' is my pet.')
