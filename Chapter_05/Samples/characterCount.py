57b492775fd4d02ed20a92813df9d7fb23cdec20
GSantos23 <santosgerson64@gmail.com>
2019-07-31 20:41:30 -0600

    Add character count

---
+++ Chapter_05/Samples/characterCount.py
@@ -0,0 +1,10 @@
+message = 'It was a bright cold day in April, and the clocks striking thirtheen.'
+count = {}
+
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+
+print(count)
