69773babd3096f1b985f2a1c8f05c69cdf744398
GSantos23 <santosgerson64@gmail.com>
2020-06-10 20:09:09 -0600

    in and not operator

---
+++ Chapter_04/samples/myPets.py
@@ -0,0 +1,8 @@
+myPets = ['Zophie', 'Pooka', 'Fat-Tail']
+print('Enter a pet name: ')
+name = input()
+
+if name not in myPets:
+	print('I do not have a pet named ' + name)
+else:
+	print(name + ' is my pet.')
